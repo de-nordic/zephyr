0f8b4ce3dfb4849a3e748de8848f4557f31503ae
Dominik Ermel <dominik.ermel@nordicsemi.no>
2022-11-02 14:24:49 +0000

    mgmt/mcumgr: Move app image information API out of img_mgmt.h
    API for obtaining image information has been moved to image_info.h.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- subsys/mgmt/mcumgr/lib/cmd/img_mgmt/src/img_mgmt_state.c
+++ subsys/mgmt/mcumgr/lib/cmd/img_mgmt/src/img_mgmt_state.c
@@ -11,6 +11,7 @@
 #include <string.h>
 #include "img_mgmt/img_mgmt.h"
 #include "img_mgmt/img_mgmt_state.h"
+#include "img_mgmt/image_info.h"
 #include "img_mgmt/image.h"
 #include "img_mgmt_priv.h"
 #include "img_mgmt/img_mgmt_impl.h"
